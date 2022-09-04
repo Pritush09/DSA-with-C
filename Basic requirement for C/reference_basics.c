18bb4c67a22052903927b7deb7aeff5deeb62484
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-04 12:43:46 +0530

    finished with refference

---
+++ Basic requirement for C/reference_basics.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a = 10;
+    int r = a; // now the r becomes the reference for the variable a it does not take any extra memory and point to the same address
+
+    //int b=9;
+    //r =b;  here the r value of a will be overwrite by the b value but the r will not behave as referrence for b it will reffer to a only
+
+    printf("%d , %d",a,r);
+    return 0;
+
+}
