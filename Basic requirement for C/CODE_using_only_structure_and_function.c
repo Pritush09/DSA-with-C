923805f6bad4a752d62c1ece85959426f721c266
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-05 20:55:25 +0530

    finishing of with the code using only functions and structure

---
+++ Basic requirement for C/CODE_using_only_structure_and_function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Rectangle{
+    int lenght;
+    int breadth;
+};
+
+int initialise(struct Rectangle *p,int x,int y){
+    p->lenght = x;
+    p->breadth = y;
+}
+
+int change_lenght(struct Rectangle *p,int l){
+    p->lenght = l;
+}
+
+int area(struct Rectangle r){
+    return (r.lenght*r.breadth);
+}
+
+int main(){
+    struct Rectangle r;
+    initialise(&r,10,20);
+    change_lenght(&r,15);
+    area(r);
+    printf("All task completed smoothly");
+}
