49f7d5722e685753293db5be81f6b61e30a71098
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-05 13:51:28 +0530

    done with functions returning an array

--- Basic requirement for C/how_the_function_can_return_an_array_as_a_parameter.c
+++ Basic requirement for C/how_the_function_can_return_an_array_as_a_parameter.c
@@ -4,6 +4,7 @@
 int * fun(int x){    // []is an old syntax and is not supported now so we use * after int * fun()
     int *p;
     p = (int *)malloc(x*sizeof(int));
+    p[0]=1;
     return (p);
 }
 //The above function is creating an array as the array is not present in main  
@@ -12,4 +13,8 @@ int * fun(int x){    // []is an old syntax and is not supported now so we use * 
 int main(){
     int *A;
     A = fun(5);
+    printf("%d\n",A);
+    for(int i =0;i<5;i++){
+        printf("%d ",A[i]);
+    }
 }
