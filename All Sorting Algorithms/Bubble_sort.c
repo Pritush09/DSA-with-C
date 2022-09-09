3aaae9c12b87c9ef639b5d5f95b05dc571da7d9d
Pritush Singh <mynameispritush.15@gmail.com>
2022-09-09 13:08:08 +0530

    done with the bubble sort

---
+++ All Sorting Algorithms/Bubble_sort.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int swap(int *x,int *y){
+    int temp;
+    temp = *x;
+    *x=*y;
+    *y=temp;
+}
+
+int bubble_sort(int A [],int n){
+    int flag; // this is for checking whether the array is already sorted or not
+    for(int i = 0;i<n-1;i++){
+        for (int j=0;j<n-1-i;j++){
+            if (A[j]>A[j+1]){
+                swap(&A[j],&A[j+1]);
+                flag=1;
+            }
+        }
+        if (flag==0){
+            break;
+        }
+    }
+}
+
+int main(){
+    int A[5]={1,4,7,3,5};
+    bubble_sort(A,5);
+    for (int i=0;i<5;i++){
+        printf("%d ",A[i]);
+    }
+}
--- Recursion/static_variable_and_global_variable.c
+++ Recursion/static_variable_and_global_variable.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 
+
+int m;//this is the global variable 
+
 int fun(int x){
-    static int n;
-    printf("%d",n);
+    static int n;// this will be created only once in the stack part of the main memory 
+    //printf("%d",n);
     if(x>0){
         n++;
         return fun(x-1)+n;
