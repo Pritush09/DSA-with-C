ff3caeec2b46ed3e4118f4fe20374e5d168c0b0c
Pritush Singh <mynameispritush.15@gmail.com>
2022-12-02 15:54:27 +0530

    done with the problems in bn search

--- Searching Algos/Binary_search_recursive.c
+++ Searching Algos/Binary_search_recursive.c
@@ -6,29 +6,28 @@ int A[10]= {1,2,3,4,5,6,7,8,8,9};
 int binary_sr(int m,int n,int num){
     int mid,no;
     mid=n+m/2;
-    no=A[mid];
     if(mid>0){
-        if(no==num){
+        if(A[mid]==num){
             return mid;
         }
         else if(no<num){
-            binary_sr(mid+1,n,num);
+            return binary_sr(mid+1,n,num);
         }
         else{
-            binary_sr(m,mid-1,num);
+            return binary_sr(m,mid-1,num);
         }
     }
     return -1;
 }
 
 int main(){
-    int num,mid;
+    int num,mi;
     printf("\nEnter the number u want to search : ");
     scanf("%d",&num);
-    mid = binary_sr(0,10,num);
-    if(mid==-1){
+    mi = binary_sr(0,10,num);
+    if(mi==-1){
         printf("\nNo. not present");
     }
     
-    printf("THe no. is found position %d",mid);
+    printf("THe no. is found position %d",mi);
 }
