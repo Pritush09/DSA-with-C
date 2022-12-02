c31151f1db403e248c2d134b1d4a1a5ea0533851
Pritush Singh <mynameispritush.15@gmail.com>
2022-12-02 15:51:30 +0530

    finishing with binary search recursive

--- Searching Algos/Binary_search_recursive.c
+++ Searching Algos/Binary_search_recursive.c
@@ -2,6 +2,33 @@
 
 int A[10]= {1,2,3,4,5,6,7,8,8,9};
 
+
+int binary_sr(int m,int n,int num){
+    int mid,no;
+    mid=n+m/2;
+    no=A[mid];
+    if(mid>0){
+        if(no==num){
+            return mid;
+        }
+        else if(no<num){
+            binary_sr(mid+1,n,num);
+        }
+        else{
+            binary_sr(m,mid-1,num);
+        }
+    }
+    return -1;
+}
+
 int main(){
-    printf("%d",(7/3));
+    int num,mid;
+    printf("\nEnter the number u want to search : ");
+    scanf("%d",&num);
+    mid = binary_sr(0,10,num);
+    if(mid==-1){
+        printf("\nNo. not present");
+    }
+    
+    printf("THe no. is found position %d",mid);
 }
