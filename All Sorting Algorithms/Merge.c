9a3174f5ab9947264e8aa3328150b027b321e188
Pritush Singh <mynameispritush.15@gmail.com>
2022-12-04 16:40:34 +0530

    finished with the merge logic

---
+++ All Sorting Algorithms/Merge.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int k[100];
+int mn=0;
+
+int merge(int A[],int M[],int a,int m){
+    int i=0,j=0;
+    while (i<m && j<a){
+        if (A[j]>M[i]){
+            k[mn]=M[i];
+            i++;
+            mn++;
+        }
+        else if (A[j]<M[i]){
+            k[mn]=A[j];
+            j++;
+            mn++;
+        }
+    }
+    while (i<m){
+        k[mn]=M[i];
+        i++;
+        mn++;
+    }
+    while(j<a){
+        k[mn]=A[j];
+        j++;
+        mn++;
+    }
+}
+
+int main(){
+    int a[3]={1,2,3};
+    int m[3]={4,5,6};
+    merge(a,m,3,3);
+    for (int i =0;i<mn;i++){
+        printf("%d ",k[i]);
+    }
+}
